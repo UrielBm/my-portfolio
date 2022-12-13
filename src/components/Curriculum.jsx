789d12561b88125aa2d55e995ae49e2d59839058
UrielBm <urielbenitez96@hotmail.com>
2022-12-12 20:06:42 -0600

    update curriculum order in component

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -6,18 +6,20 @@ import Description from "./CV/Description"
 import WrapperWorkExp from "./CV/WrapperWorkExp"
 import WrapperSkills from "./CV/WrapperSkills"
 import pdf from "../components/assets/Cv Benitez Medina Uriel.pdf"
+import WrapperAptitudes from "./CV/WrapperAptitudes"
 const Curriculum = () => {
   return (
     <>
       <div className="my_cv">
         <div className="my_cvLeft">
           <Perfil />
+          <WrapperWorkExp />
           <WrapperSchool />
-          <WrapperContact />
         </div>
         <div className="my_cvRight">
+          <WrapperContact />
+          <WrapperAptitudes />
           <Description />
-          <WrapperWorkExp />
           <WrapperSkills />
         </div>
       </div>
